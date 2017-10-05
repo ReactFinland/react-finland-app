68705ca25ccb79031a3fb8968b558b89d065631c
Toni Ristola <toni.ristola@gofore.com>
2017-10-05 22:14:04 +0300

    Refactored the stories to work with hot reload

--- App/Components/ContributorCard.story.js
+++ App/Components/ContributorCard.story.js
@@ -4,12 +4,17 @@ import { storiesOf } from '@storybook/react-native'
 
 import ContributorCard from './ContributorCard'
 
+class DefaultStory extends React.Component {
+  render() {
+    return (
+      <View style={{backgroundColor: 'black', flex: 1, justifyContent: 'center'}}>
+        <ContributorCard
+          author={'Aleksi Pousar'}
+          picture={'https://avatars0.githubusercontent.com/u/4244526?v=4&s=460'}
+        />
+      </View>
+    )
+  }
+}
 storiesOf('ContributorCard')
-  .add('Default', () => (
-    <View style={{backgroundColor: 'black', flex: 1, justifyContent: 'center'}}>
-      <ContributorCard
-        author={'Aleksi Pousar'}
-        picture={'https://avatars0.githubusercontent.com/u/4244526?v=4&s=460'}
-      />
-    </View>
-  ))
+  .add('Default', () => <DefaultStory/>)
--- App/Components/TalkCard.story.js
+++ App/Components/TalkCard.story.js
@@ -4,14 +4,19 @@ import { storiesOf } from '@storybook/react-native'
 
 import TalkCard from './TalkCard'
 
+class DefaultStory extends React.Component {
+  render() {
+    return (
+      <View style={{backgroundColor: 'black', flex: 1, justifyContent: 'center'}}>
+        <TalkCard
+          author={'Toni Ristola'}
+          picture={'https://avatars3.githubusercontent.com/u/538833?v=4&s=460'}
+          title={'Talk test'}
+          time={'to be revealed'}
+        />
+      </View>
+    )
+  }
+}
 storiesOf('TalkCard')
-  .add('Default', () => (
-    <View style={{backgroundColor: 'black', flex: 1, justifyContent: 'center'}}>
-      <TalkCard
-        author={'Toni Ristola'}
-        picture={'https://avatars3.githubusercontent.com/u/538833?v=4&s=460'}
-        title={'Talk test'}
-        time={'to be revealed'}
-      />
-    </View>
-  ))
+  .add('Default', () => <DefaultStory/>)
