b04cdbea51571d919cc1738a2d66f4d7255f6dac
Joni Nevalainen <joni.nevalainen@gofore.com>
2017-10-08 19:27:53 +0300

    Improve ContributorCard

--- App/Components/ContributorCard.js
+++ App/Components/ContributorCard.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import styled from 'styled-components/native'
+import Icon from 'react-native-vector-icons/FontAwesome'
 
-import { Fonts, Colors } from '../Themes'
+import { Fonts, Colors, Metrics } from '../Themes'
 
 const Container = styled.View`
   background-color: ${Colors.snow};
+  border-radius: 10;
 `
 
 const Row = styled.View`
@@ -17,16 +19,20 @@ const Author = styled.Text`
   font-family: ${Fonts.type.bold};
 `
 
+const About = styled.Text`
+  color: ${Colors.text};
+  font-size: ${Fonts.size.regular};
+`
+
 const TalkInfo = styled.View`
   flex: 7;
   justify-content: center;
-  padding-left: 15;
+  padding: 15px;
 `
 
 const ImageContainer = styled.View`
   flex: 1;
   align-items: center;
-  justify-content: center;
   padding-right: 15;
   padding-bottom: 10;
   padding-top: 10;
@@ -40,19 +46,39 @@ const RoundedImage = styled.Image`
   border-color: ${Colors.grey}
 `
 
+const Social = styled.View`
+  background-color: ${Colors.grey};
+  padding: 15px;
+  border-bottom-left-radius: 10px;
+  border-bottom-right-radius: 10px;
+  flex-direction: row;
+  justify-content: flex-end;
+  align-items: center;
+`
+
+const Spacing = styled.View`
+  margin-left: 20;
+`
+
 const ContributorCard = (props) => {
-  const { author, picture } = props
+  const { author, picture, about } = props
   return (
     <Container>
       <Row>
         <TalkInfo>
           <Author>{ author }</Author>
+          <About>{ about }</About>
         </TalkInfo>
         <ImageContainer>
           <RoundedImage source={{uri: picture}} />
         </ImageContainer>
       </Row>
-    </Container>
+      <Social>
+        <Icon name='twitter' size={Metrics.icons.medium} />
+        <Spacing />
+        <Icon name='github' size={Metrics.icons.medium} />
+      </Social>
+  </Container>
   )
 }
 
