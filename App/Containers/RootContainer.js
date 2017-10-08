78090f5e379e917a61f2b6483797e90b6355d594
Joni Nevalainen <joni.nevalainen@gofore.com>
2017-10-08 11:49:24 +0300

    Rename AppNavigation to PrimaryNavigation

--- App/Containers/RootContainer.js
+++ App/Containers/RootContainer.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import { View, StatusBar } from 'react-native'
-import ReduxNavigation from '../Navigation/ReduxNavigation'
+import ReduxPrimaryNavigation from '../Navigation/ReduxPrimaryNavigation'
 import { connect } from 'react-redux'
 import StartupActions from '../Redux/StartupRedux'
 import ReduxPersist from '../Config/ReduxPersist'
@@ -20,7 +20,7 @@ class RootContainer extends Component {
     return (
       <View style={styles.applicationView}>
         <StatusBar barStyle='light-content' />
-        <ReduxNavigation />
+        <ReduxPrimaryNavigation />
       </View>
     )
   }
--- App/Redux/index.js
+++ App/Redux/index.js
@@ -5,7 +5,7 @@ import rootSaga from '../Sagas/'
 export default () => {
   /* ------------- Assemble The Reducers ------------- */
   const rootReducer = combineReducers({
-    nav: require('./NavigationRedux').reducer,
+    primaryNav: require('./PrimaryNavigationRedux').reducer,
     github: require('./GithubRedux').reducer,
     search: require('./SearchRedux').reducer,
     schedule: require('./ScheduleRedux').reducer
