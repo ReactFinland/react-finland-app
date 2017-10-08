0b5daef615e1943a6947a1bdf677ae7df39b1fd0
Joni Nevalainen <joni.nevalainen@gofore.com>
2017-10-08 13:03:16 +0300

    Update schedule based on the screen user navigates to

--- App/Redux/ScheduleRedux.js
+++ App/Redux/ScheduleRedux.js
@@ -1,16 +1,39 @@
 import { createReducer } from 'reduxsauce'
 import Immutable from 'seamless-immutable'
+import { NavigationActions } from 'react-navigation'
 
 const mapSpeakersToSchedule = (schedule, speakers) => {
   return schedule.map(s => {
     return {...s, author: speakers[s.author]}
   })
 }
-export const INITIAL_STATE = Immutable({
-  schedule: mapSpeakersToSchedule(
-    require('../Fixtures/schedule.json').schedule,
-    require('../Fixtures/schedule.json').speakers
-  )}
+
+const fullSchedule = mapSpeakersToSchedule(
+  require('../Fixtures/schedule.json').schedule,
+  require('../Fixtures/schedule.json').speakers
 )
 
-export const reducer = createReducer(INITIAL_STATE, {})
+const wednesdaySchedule = fullSchedule.filter((s) => true)
+const thursdaySchedule = fullSchedule.filter((s) => false)
+
+export const INITIAL_STATE = Immutable({
+  schedule: wednesdaySchedule
+})
+
+/* ------------- Reducers ------------- */
+
+const updateSchedule = (state, { routeName }) => {
+  if (routeName === 'WednesdayScreen') {
+    return state.setIn(['schedule'], wednesdaySchedule)
+  } else if (routeName === 'ThursdayScreen') {
+    return state.setIn(['schedule'], thursdaySchedule)
+  } else {
+    return state
+  }
+}
+
+/* ------------- Hookup Reducers To Types ------------- */
+
+export const reducer = createReducer(INITIAL_STATE, {
+  [NavigationActions.NAVIGATE]: updateSchedule
+})
