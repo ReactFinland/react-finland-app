e35c16735a62a037bbe27215fc7f48473f8b9357
Toni Ristola <toni.ristola@gofore.com>
2017-10-28 15:07:34 +0300

    move ReactFinlandLogo to a folder

--- App/Components/Stories.js
+++ App/Components/Stories.js
@@ -1,4 +1,4 @@
-import './AboutText'.Story
-import './TalkCard.story'
-import './SpeakerCard.story'
-import './ReactFinlandLogo.story'
+require('./AboutText').Story
+require('./TalkCard').Story
+require('./SpeakerCard').Story
+require('./ReactFinlandLogo').Story
