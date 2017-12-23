904c19771897d341918e87a8866e4b3b983f94e7
Joni Nevalainen <joninevalainen@gmail.com>
2017-12-23 13:06:49 +0200

    Add initial talk detail component with title and description

---
+++ App/Components/TalkDetail/TalkDetail.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import styled from 'styled-components/native'
+
+import { Colors, Fonts, Metrics } from '../../Themes'
+
+const Container = styled.View`
+  background-color: ${Colors.snow};
+  padding: ${Metrics.doubleBaseMargin}px;
+`
+
+const Top = styled.View`
+  padding: ${Metrics.doubleBaseMargin}px;
+`
+
+const Bottom = styled.View`
+  padding: ${Metrics.doubleBaseMargin}px;
+`
+
+const Title = styled.Text`
+  color: ${Colors.text};
+  font-size: ${Fonts.size.h3};
+  font-family: ${Fonts.type.bold};
+  text-align: center;
+`
+
+const Description = styled.Text`
+  color: ${Colors.text};
+  font-size: ${Fonts.size.regular};
+`
+
+const TalkDetail = ({ title, description, speakers }) => (
+  <Container>
+    <Top>
+      <Title>{title}</Title>
+    </Top>
+    <Bottom>
+      <Description>{description}</Description>
+    </Bottom>
+  </Container>
+)
+
+export default TalkDetail
---
+++ App/Components/TalkDetail/index.js
@@ -0,0 +1,7 @@
+import TalkDetail from './TalkDetail'
+import Story from './TalkDetail.story'
+export {
+   Story
+}
+
+export default TalkDetail
