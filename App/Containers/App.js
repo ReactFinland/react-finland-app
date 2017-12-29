00bff23c978db8de7f60f13c41f4775f27df73b2
Toni Ristola <toni.ristola@gofore.com>
2017-12-29 10:49:57 +0200

    Refactor main app

--- App/Containers/App.js
+++ App/Containers/App.js
@@ -8,71 +8,16 @@ import createStore from '../Redux'
 import ApolloClient from 'apollo-client-preset'
 import { ApolloProvider } from 'react-apollo'
 import { HttpLink } from 'apollo-link-http';
-import { ReduxCache, apolloReducer } from 'apollo-cache-redux';
-import gql from 'graphql-tag';
 
 // create our store
+const store = createStore()
 
-const store = createStore(apolloReducer)
-const cache = new ReduxCache({ store });
-const client = new ApolloClient({
-  link: new HttpLink({ uri: 'https://api.react-finland.fi/graphql-2018' }),
-  cache
-})
 class App extends Component {
-  componentDidMount() {
-    console.tron.log('do query')
-    client.query({
-      query: gql`
-      {
-
-        organizers {
-          name
-          location {
-            country {
-              name
-              code
-            }
-          }
-        }
-        speakers {
-          name
-          keywords
-        },
-        schedules {
-          day,
-          intervals {
-            begin
-            end
-            sessions {
-              description
-            }
-          }
-        },
-        tickets {
-          name
-          link
-        }
-      }
-      `,
-    })
-      .then(data => 
-        {
-          console.tron.log(data)
-        }
-      )
-      .catch(error => console.tron.error(error))
-
-  }
-
   render () {
     return (
-      <ApolloProvider client={client}>
-        <Provider store={store}>
+      <Provider store={store}>
           <RootContainer />
-        </Provider>
-      </ApolloProvider>
-
+      </Provider>
     )
   }
 }
--- App/Redux/index.js
+++ App/Redux/index.js
@@ -6,7 +6,6 @@ import rootSaga from '../Sagas/'
 export default apollo => {
   /* ------------- Assemble The Reducers ------------- */
   const rootReducer = combineReducers({
-    apollo,
     primaryNav: require('./PrimaryNavigationRedux').reducer,
     scheduleNav: require('./ScheduleNavigationRedux').reducer,
     schedule: require('./ScheduleRedux').reducer
