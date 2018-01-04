0c34474769c6b98a8ba2ba27de0cf895a7b2ac05
Toni Ristola <toni.ristola@gofore.com>
2018-01-04 21:30:19 +0200

    move backrow to separate ui component

--- App/Components/TalkDetail/TalkDetail.js
+++ App/Components/TalkDetail/TalkDetail.js
@@ -3,7 +3,7 @@ import styled from 'styled-components/native'
 import { TouchableOpacity } from 'react-native'
 import SpeakerList from './SpeakerList'
 import { Colors, Fonts, Metrics } from '../../Themes'
-import Icon from 'react-native-vector-icons/EvilIcons'
+import Icon from 'react-native-vector-icons/Ionicons'
 
 const Container = styled.ScrollView`
   background-color: ${Colors.snow};
@@ -13,34 +13,41 @@ const Container = styled.ScrollView`
 const Top = styled.View`
   padding: ${Metrics.doubleBaseMargin}px;
 `
-
 const Bottom = styled.View`
   padding: ${Metrics.doubleBaseMargin}px;
 `
-const TopRow = styled.View`
-  flex: 1;
-  flex-direction: row;
-  justify-content: flex-start;
-`
+
 const Title = styled.Text`
   color: ${Colors.text};
   font-size: ${Fonts.size.h3};
   font-family: ${Fonts.type.bold};
   text-align: center;
 `
-
+const Back = styled.Text`
+color: ${Colors.text};
+font-size: ${Fonts.size.h5};
+`
 const Description = styled.Text`
   color: ${Colors.text};
   font-size: ${Fonts.size.regular};
 `
+const TopRow = styled.View`
+  flex: 1;
+  flex-direction: row;
+  justify-content: flex-start;
+`
+
+const BackRow = () => (
+  <TopRow>
+    <Icon style={{paddingRight: 10, paddingBottom: 10}} size={28} name='ios-arrow-round-back' />
+    <Back>Back</Back>
+  </TopRow>
+)
 
 const TalkDetail = ({ title, description, speakers, navigation }) => (
   <Container>
     <TouchableOpacity onPress={() => navigation.goBack()}>
-      <TopRow>
-        <Icon size={24} name='arrow-left' />
-        <Description>Back</Description>
-      </TopRow>
+      <BackRow />
     </TouchableOpacity>
     <Top>
       <Title>{title}</Title>
