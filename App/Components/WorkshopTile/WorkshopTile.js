f47b39ec9495aa87db334c90d0dd6761461c7ae7
Toni Ristola <toni.ristola@gofore.com>
2018-01-14 10:04:10 +0200

    Animation to onPress

--- App/Components/WorkshopTile/WorkshopTile.js
+++ App/Components/WorkshopTile/WorkshopTile.js
@@ -1,7 +1,9 @@
 import React from 'react'
-import { TouchableHighlight } from 'react-native'
+import { TouchableWithoutFeedback } from 'react-native'
+import * as Animatable from 'react-native-animatable'
 import styled from 'styled-components/native'
 import { Fonts, Colors, Metrics } from '../../Themes'
+import R from 'ramda'
 
 const Row = styled.View`
   border-radius: 10px;
@@ -26,14 +28,44 @@ const Desc = styled.Text`
   font-size: 15px;
   color: ${Colors.facebook};
 `
-
-const WorkshopTile = ({item, onPress}) => (
-  <TouchableHighlight onPress={onPress}>
-    <Row>
-      <Headline numberOfLines={2}>{item && item.title}</Headline>
-      <Desc numberOfLines={3}>{item && item.description}</Desc>
-    </Row>
-  </TouchableHighlight>
+const ImageContainer = styled.View`
+  position: absolute;
+  right: 5px;
+  top: 5px;
+`
+const RoundedImage = styled.Image`
+  width: 50;
+  height: 50;
+  border-radius: 25px;
+  border-width: 1px;
+  border-color: ${Colors.grey}
+`
+const getImage = (speakers) => (
+  speakers && speakers.length > 0
+  ? <RoundedImage source={{uri: `https://api.react-finland.fi/graphql-2018/images/${speakers[0].image}`}} />
+  : null
 )
+
+const StyledRow = Animatable.createAnimatableComponent(Row)
+class WorkshopTile extends React.Component {
+  onPress = () => {
+    const { onPress } = this.props
+    this.refs.view.pulse(300).then((endState) => onPress && onPress())
+  }
+  render () {
+    const {item} = this.props
+    return (
+      <TouchableWithoutFeedback onPress={this.onPress}>
+        <StyledRow ref='view'>
+          <Headline numberOfLines={2}>{item && item.title}</Headline>
+          <Desc numberOfLines={3}>{item && item.description}</Desc>
+          <ImageContainer>
+            {getImage(R.pathOr([], ['speakers'], item))}
+          </ImageContainer>
+        </StyledRow>
+      </TouchableWithoutFeedback>
+    )
+  }
+}
 
 export default WorkshopTile
