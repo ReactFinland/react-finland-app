a4b8f8e51485c87cee2ad3cf50360051b11aee92
Toni Ristola <toni.ristola@gofore.com>
2018-01-31 16:14:35 +0200

    drafting backgroudns

--- App/Components/TalkDetail/BackRow.js
+++ App/Components/TalkDetail/BackRow.js
@@ -9,9 +9,11 @@ const backIcon = Platform.OS === 'ios' ? 'ios-arrow-back' : 'ios-arrow-round-bac
 const Back = styled.Text`
   color: ${Colors.text};
   font-size: ${Fonts.size.h5};
+  background-color: 'rgba(0,0,0,0)';
 `
 const TopRow = styled.View`
   flex: 1;
+  background-color: 'rgba(0,0,0,0)';
   flex-direction: row;
   justify-content: space-between;
 `
@@ -21,8 +23,8 @@ const openUrl = link => Linking.openURL(link).catch(err => {
 })
 export const BackRow = ({onBack}) => (
   <TopRow>
-    <TouchableOpacity style={{flexDirection: 'row', justifyContent: 'flex-start'}} onPress={() => onBack()}>
-      <Icon style={{paddingRight: 10, paddingBottom: 10}} size={28} name={backIcon} />
+    <TouchableOpacity style={{height: 20, flexDirection: 'row', justifyContent: 'flex-start'}} onPress={() => onBack()}>
+      <Icon style={{paddingRight: 10, paddingBottom: 10, backgroundColor: 'rgba(0,0,0,0)'}} size={28} name={backIcon} />
       <Back>Back</Back>
     </TouchableOpacity>
     <TouchableOpacity onPress={() => openUrl('https://ti.to/react-finland/2018')}>
--- App/Components/TalkDetail/TalkDetail.js
+++ App/Components/TalkDetail/TalkDetail.js
@@ -1,12 +1,19 @@
 import React from 'react'
 import styled from 'styled-components/native'
+import { Text, Dimensions, TouchableHighlight } from 'react-native'
+
 import SpeakerList from './SpeakerList'
 import { Colors, Fonts, Metrics } from '../../Themes'
 import { BackRow } from './BackRow'
 
+const BackgroundImage = styled.Image`
+  flex: 1;
+  width: ${props => props.width};
+`
 const Container = styled.ScrollView`
   background-color: ${Colors.snow};
   padding: ${Metrics.baseMargin}px;
+  background-color: 'rgba(0,0,0,0)';
   border-radius: 10;
 `
 
@@ -22,26 +29,46 @@ const Title = styled.Text`
   font-size: ${Fonts.size.h3};
   font-family: ${Fonts.type.bold};
   text-align: center;
+  background-color: 'rgba(0,0,0,0)';
 `
 
 const Description = styled.Text`
   color: ${Colors.text};
   font-size: ${Fonts.size.regular};
   font-family: ${Fonts.type.base};
-  color: ${Colors.charcoal};
+  color: ${Colors.snow};
+  background-color: 'rgba(0,0,0,0)';
 `
 
-const TalkDetail = ({ title, description, speakers, navigation }) => (
-  <Container>
-    <BackRow onBack={navigation.goBack} />
-    <Top>
-      <Title>{title}</Title>
-    </Top>
-    {speakers && <SpeakerList speakers={speakers} />}
-    <Bottom>
-      <Description>{description}</Description>
-    </Bottom>
-  </Container>
-)
-
+class TalkDetail extends React.Component {
+  constructor (props) {
+    super(props)
+    const { width } = Dimensions.get('window')
+    this.state = { width }
+    this.onLayout = this.onLayout.bind(this)
+  }
+  onLayout (e) {
+    const { width } = Dimensions.get('window')
+    this.setState({ width })
+  }
+  render() {
+    const { width } = this.state
+    const { title, description, speakers, navigation } = this.props
+    return(
+      <Container>
+        <BackgroundImage width={width} source={require('../../Images/react-finland-background.png')} >
+        <BackRow onBack={navigation.goBack} />
+        <Top>
+        <Title>{title}</Title>
+        </Top>
+        {speakers && <SpeakerList speakers={speakers} />}
+        <Bottom>
+        <Description>{description}</Description>
+        </Bottom>
+        </BackgroundImage>
+      </Container>
+    )
+  }
+}
+    
 export default TalkDetail
