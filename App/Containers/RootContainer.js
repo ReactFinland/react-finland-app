6bf7df3158941804197dd945190625a31f5ac84d
Joni Nevalainen <joninevalainen@gmail.com>
2018-02-12 21:52:48 +0200

    Handle Android back button

--- App/Containers/RootContainer.js
+++ App/Containers/RootContainer.js
@@ -1,14 +1,46 @@
 import React, { Component } from 'react'
-import { View, StatusBar } from 'react-native'
+import { View, StatusBar, BackHandler } from 'react-native'
 import ReduxPrimaryNavigation from '../Navigation/ReduxPrimaryNavigation'
 import { connect } from 'react-redux'
 import StartupActions from '../Redux/StartupRedux'
 import ReduxPersist from '../Config/ReduxPersist'
+import { NavigationActions } from 'react-navigation'
 
 // Styles
 import styles from './Styles/RootContainerStyles'
 
+// https://github.com/react-navigation/react-navigation/issues/1349#issuecomment-304692012
+const isRootScreen = (navigator) => {
+  if (navigator.index == null) {
+    return true;
+  }
+
+  if (navigator.index > 0) {
+    return false;
+  }
+
+  return !navigator.routes || !navigator.routes.find(route => !isRootScreen(route));
+}
+
 class RootContainer extends Component {
+  componentWillMount() {
+    BackHandler.addEventListener('hardwareBackPress', this.onBackButtonPressed)
+  }
+
+  componentWillUnmount() {
+    BackHandler.removeEventListener('hardwareBackPress', this.onBackButtonPressed)
+  }
+
+  onBackButtonPressed = () => {
+    if (isRootScreen(this.props.navigation)) {
+      // let react-navigation pop the screen
+      return false
+    }
+
+    this.props.goBack()
+    return true
+  }
+
   componentDidMount () {
     if (!ReduxPersist.active) {
       this.props.startup()
@@ -25,9 +57,14 @@ class RootContainer extends Component {
   }
 }
 
-// wraps dispatch to create nicer functions to call within our component
-const mapDispatchToProps = (dispatch) => ({
-  startup: () => dispatch(StartupActions.startup())
+const mapStateToProps = (state) => ({
+  navigation: state.primaryNav
 })
 
-export default connect(null, mapDispatchToProps)(RootContainer)
+// wraps dispatch to create nicer functions to call within our component
+const mapDispatchToProps = (dispatch) => ({
+  startup: () => dispatch(StartupActions.startup()),
+  goBack: () => dispatch(NavigationActions.back())
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(RootContainer)
