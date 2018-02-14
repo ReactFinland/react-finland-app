5365ab8e2d6953afa43d20ecc2a21d1c6d03d1e3
Aarni Koskela <akx@iki.fi>
2018-02-14 11:32:09 +0200

    Simplify AboutScreen to an ImageBackground and a ScrollView

--- App/Containers/AboutScreen.js
+++ App/Containers/AboutScreen.js
@@ -1,46 +1,19 @@
 import React, { Component } from 'react'
-import { Dimensions, ImageBackground } from 'react-native'
+import { ImageBackground, ScrollView } from 'react-native'
 
 import AboutText from '../Components/AboutText'
-import styled from 'styled-components/native'
-import Icon from 'react-native-vector-icons/FontAwesome'
-
-import { Metrics, Colors } from '../Themes'
-
-const Container = styled.ScrollView`
-  flex: 1;
-  background-color: ${Colors.background};
-`
 
 export default class AboutScreen extends Component {
-  static navigationOptions = {
-    tabBarLabel: 'About',
-    tabBarIcon: ({ tintColor }) =>
-      <Icon name='info-circle' size={Metrics.icons.tiny} color={tintColor} />
-  }
-
-  constructor (props) {
-    super(props)
-    const { width, height } = Dimensions.get('window')
-    this.state = { width, height }
-  }
-
-  onLayout = (e) => {
-    const { width, height } = Dimensions.get('window')
-    this.setState({ width, height })
-  }
-
   render () {
-    const { width, height } = this.state
     return (
-      <Container onLayout={this.onLayout}>
-        <ImageBackground
-          style={{ width, minHeight: height - Metrics.navBarHeight }}
-          source={require('../Images/react-finland-background.png')}
-        >
+      <ImageBackground
+        style={{ flex: 1 }}
+        source={require('../Images/react-finland-background.png')}
+      >
+        <ScrollView style={{ flex: 1 }}>
           <AboutText />
-        </ImageBackground>
-      </Container>
+        </ScrollView>
+      </ImageBackground>
     )
   }
 }
