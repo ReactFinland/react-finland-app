79d988a3fea143a625d58912a2eed4dad4997571
Aarni Koskela <akx@iki.fi>
2018-02-14 11:19:07 +0200

    Upgrade to React Native 0.53

---
+++ rn-cli.config.js
@@ -0,0 +1,8 @@
+const blacklist = require('metro/src/blacklist')
+
+// h/t https://github.com/oblador/react-native-vector-icons/issues/626#issuecomment-357469857
+module.exports = {
+  getBlacklistRE () {
+	return blacklist([/react-native\/local-cli\/core\/__fixtures__.*/])
+  },
+}
