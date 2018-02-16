5991049d99f83bb7e95cc1a9832c2cfd8f508711
Aarni Koskela <akx@iki.fi>
2018-02-16 10:45:48 +0200

    `yarn fixcode`

--- App/Containers/RootContainer.js
+++ App/Containers/RootContainer.js
@@ -12,22 +12,22 @@ import styles from './Styles/RootContainerStyles'
 // https://github.com/react-navigation/react-navigation/issues/1349#issuecomment-304692012
 const isRootScreen = (navigator) => {
   if (navigator.index == null) {
-    return true;
+    return true
   }
 
   if (navigator.index > 0) {
-    return false;
+    return false
   }
 
-  return !navigator.routes || !navigator.routes.find(route => !isRootScreen(route));
+  return !navigator.routes || !navigator.routes.find(route => !isRootScreen(route))
 }
 
 class RootContainer extends Component {
-  componentWillMount() {
+  componentWillMount () {
     BackHandler.addEventListener('hardwareBackPress', this.onBackButtonPressed)
   }
 
-  componentWillUnmount() {
+  componentWillUnmount () {
     BackHandler.removeEventListener('hardwareBackPress', this.onBackButtonPressed)
   }
 
--- rn-cli.config.js
+++ rn-cli.config.js
@@ -3,6 +3,6 @@ const blacklist = require('metro/src/blacklist')
 // h/t https://github.com/oblador/react-native-vector-icons/issues/626#issuecomment-357469857
 module.exports = {
   getBlacklistRE () {
-	return blacklist([/react-native\/local-cli\/core\/__fixtures__.*/])
-  },
+    return blacklist([/react-native\/local-cli\/core\/__fixtures__.*/])
+  }
 }
