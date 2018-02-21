cd98a180ae248c56ed07679dde434f5c886a418a
Toni Ristola <toni.ristola@gofore.com>
2018-02-21 14:18:56 +0200

    centering a bit and making it look better

--- App/Components/SponsorList/SponsorList.js
+++ App/Components/SponsorList/SponsorList.js
@@ -4,36 +4,46 @@ import styled from 'styled-components/native'
 import { Fonts } from '../../Themes'
 
 export const GoldImage = styled.Image`
- margin-left: 10px;
  height: 50px;
- width: 150;
+ width: 140;
 `
-export const SilverImage = styled.Image`
- margin-left: 20px;
- height: 50px;
+export const AlmaImage = styled.Image`
+ margin-left: 0px;
+ height: 30px;
  width: 75;
+`
+
+export const SilverImage = styled.Image`
+ height: 50px;
+ width: 80;
 `
 export const BronzeImage = styled.Image`
  margin-left: 20px;
- height: 25px;
- width: 50;
+ height: 35px;
+ width: 65;
 `
-export const VerkkisImage = styled.Image`
- margin-top: 5px;
+export const VerkkokauppaImage = styled.Image`
+ margin-top: 15px;
  height: 20px;
  width: 150;
 `
-const BackgroundImage = styled.ImageBackground`
+export const BackgroundImage = styled.ImageBackground`
   flex: 1;
   justify-content: space-between;
 `
+export const SponsorContainer = styled.View`
+  margin: 10px;
+  padding-bottom: 10px;
+  padding-left: 5px;
+  padding-right: 5px;
+  background-color: rgba(255, 255, 255, 0.3);
+`
 
-const Text = styled.Text`
+export const Text = styled.Text`
   color: ${props => props.textcolor};
   padding-left: 20;
   padding-bottom: 15px;
   padding-right: 30;
-  padding-top: 50;
   text-align: center;
   font-size: ${Fonts.size.h5};
   font-family: ${Fonts.type.base};
@@ -44,26 +54,33 @@ class AboutText extends React.Component {
     return (
       <BackgroundImage rexizeMode='cover' source={require('../../Images/react-finland-background.png')} >
         <ScrollView>
-          <Text textcolor={'#ffd700'}>Gold Sponsors</Text>
-          <View style={{flexDirection: 'row'}}>
-            <GoldImage resizeMode='contain' source={require('../../Images/gofore.png')} />
-            <GoldImage resizeMode='contain' source={require('../../Images/solita.png')} />
-          </View>
-          <Text textcolor='#c0c0c0'>Silver</Text>
-          <View style={{flexDirection: 'row'}}>
-            <SilverImage resizeMode='contain' source={require('../../Images/elisa.png')} />
-            <SilverImage resizeMode='contain' source={require('../../Images/nitor.png')} />
-            <SilverImage resizeMode='contain' source={require('../../Images/motley.png')} />
-          </View>
-          <Text textcolor='#CD7F32' >Bronze</Text>
-          <View style={{justifyContent: 'center', flexDirection: 'row'}}>
-            <BronzeImage resizeMode='contain' source={require('../../Images/alma.png')} />
-            <BronzeImage resizeMode='contain' source={require('../../Images/geniem.png')} />
-            <BronzeImage resizeMode='contain' source={require('../../Images/rohea.png')} />
-          </View>
-          <View style={{justifyContent: 'center', flexDirection: 'row'}}>
-            <VerkkisImage resizeMode='contain' source={require('../../Images/verkkokauppa.png')} />
-          </View>
+          <SponsorContainer>
+            <Text textcolor={'#ffd700'}>Gold</Text>
+            <View style={{justifyContent: 'space-between', flexDirection: 'row'}}>
+              <GoldImage resizeMode='contain' source={require('../../Images/gofore.png')} />
+              <GoldImage resizeMode='contain' source={require('../../Images/solita.png')} />
+            </View>
+          </SponsorContainer>
+          <SponsorContainer>
+            <Text textcolor='#c0c0c0'>Silver</Text>
+            <View style={{justifyContent: 'space-between', flexDirection: 'row'}}>
+              <SilverImage resizeMode='contain' source={require('../../Images/elisa.png')} />
+              <SilverImage resizeMode='contain' source={require('../../Images/nitor.png')} />
+              <SilverImage resizeMode='contain' source={require('../../Images/motley.png')} />
+            </View>
+          </SponsorContainer>
+          <SponsorContainer>
+
+            <Text textcolor='#CD7F32' >Bronze</Text>
+            <View style={{justifyContent: 'space-between', flexDirection: 'row'}}>
+              <AlmaImage resizeMode='contain' source={require('../../Images/alma.png')} />
+              <BronzeImage resizeMode='contain' source={require('../../Images/geniem.png')} />
+              <BronzeImage resizeMode='contain' source={require('../../Images/rohea.png')} />
+            </View>
+            <View style={{justifyContent: 'center', flexDirection: 'row'}}>
+              <VerkkokauppaImage resizeMode='contain' source={require('../../Images/verkkokauppa.png')} />
+            </View>
+          </SponsorContainer>
         </ScrollView>
       </BackgroundImage>
     )
