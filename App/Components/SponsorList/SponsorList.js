6f4e1ac26b8f6377917298ef34527c928e6ba980
Toni Ristola <toni.ristola@gofore.com>
2018-03-18 11:19:53 +0200

    Remove opacue white background

--- App/Components/SponsorList/SponsorList.js
+++ App/Components/SponsorList/SponsorList.js
@@ -32,11 +32,11 @@ export const BackgroundImage = styled.ImageBackground`
   justify-content: space-between;
 `
 export const SponsorContainer = styled.View`
-  margin: 10px;
-  padding-bottom: 10px;
-  padding-left: 5px;
-  padding-right: 5px;
-  background-color: rgba(255, 255, 255, 0.3);
+  flex: 1; 
+  padding-top: 20px;
+  padding-bottom: 20px;
+  padding-left: 10px;
+  padding-right: 10px;
 `
 
 export const Text = styled.Text`
@@ -53,24 +53,19 @@ class AboutText extends React.Component {
   render () {
     return (
       <BackgroundImage rexizeMode='cover' source={require('../../Images/react-finland-background.png')} >
+      <SponsorContainer>
         <ScrollView>
-          <SponsorContainer>
             <Text textcolor={'#ffd700'}>Gold</Text>
             <View style={{justifyContent: 'space-between', flexDirection: 'row'}}>
               <GoldImage resizeMode='contain' source={require('../../Images/gofore.png')} />
               <GoldImage resizeMode='contain' source={require('../../Images/solita.png')} />
             </View>
-          </SponsorContainer>
-          <SponsorContainer>
             <Text textcolor='#c0c0c0'>Silver</Text>
             <View style={{justifyContent: 'space-between', flexDirection: 'row'}}>
               <SilverImage resizeMode='contain' source={require('../../Images/elisa.png')} />
               <SilverImage resizeMode='contain' source={require('../../Images/nitor.png')} />
               <SilverImage resizeMode='contain' source={require('../../Images/motley.png')} />
             </View>
-          </SponsorContainer>
-          <SponsorContainer>
-
             <Text textcolor='#CD7F32' >Bronze</Text>
             <View style={{justifyContent: 'space-between', flexDirection: 'row'}}>
               <AlmaImage resizeMode='contain' source={require('../../Images/alma.png')} />
@@ -80,8 +75,8 @@ class AboutText extends React.Component {
             <View style={{justifyContent: 'center', flexDirection: 'row'}}>
               <VerkkokauppaImage resizeMode='contain' source={require('../../Images/verkkokauppa.png')} />
             </View>
-          </SponsorContainer>
         </ScrollView>
+          </SponsorContainer>
       </BackgroundImage>
     )
   }
