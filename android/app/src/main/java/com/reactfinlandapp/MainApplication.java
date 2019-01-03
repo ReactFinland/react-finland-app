39f1e0425e3d1c3dc2dcbc8cc7ca36ae309f1a0e
Toni Ristola <toni.ristola@robu.fi>
2019-01-03 22:16:17 +0200

    updated to newer i18n version

--- android/app/src/main/java/com/reactfinlandapp/MainApplication.java
+++ android/app/src/main/java/com/reactfinlandapp/MainApplication.java
@@ -3,8 +3,9 @@ package com.reactfinlandapp;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
-import com.oblador.vectoricons.VectorIconsPackage;
+import com.AlexanderZaytsev.RNI18n.RNI18nPackage;
 import com.i18n.reactnativei18n.ReactNativeI18n;
+import com.oblador.vectoricons.VectorIconsPackage;
 import com.lugg.ReactNativeConfig.ReactNativeConfigPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -26,8 +27,9 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new VectorIconsPackage(),
+            new RNI18nPackage(),
             new ReactNativeI18n(),
+            new VectorIconsPackage(),
             new ReactNativeConfigPackage()
       );
     }
