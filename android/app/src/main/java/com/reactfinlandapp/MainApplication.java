82c471d964a6304d0bebdb6420fa8712efc9675d
Toni Ristola <toni.ristola@robu.fi>
2019-01-03 11:19:34 +0200

    temp

--- android/app/src/main/java/com/reactfinlandapp/MainApplication.java
+++ android/app/src/main/java/com/reactfinlandapp/MainApplication.java
@@ -3,9 +3,9 @@ package com.reactfinlandapp;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
-import com.lugg.ReactNativeConfig.ReactNativeConfigPackage;
-import com.i18n.reactnativei18n.ReactNativeI18n;
 import com.oblador.vectoricons.VectorIconsPackage;
+import com.i18n.reactnativei18n.ReactNativeI18n;
+import com.lugg.ReactNativeConfig.ReactNativeConfigPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -26,10 +26,15 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new ReactNativeConfigPackage(),
+            new VectorIconsPackage(),
             new ReactNativeI18n(),
-            new VectorIconsPackage()
+            new ReactNativeConfigPackage()
       );
+    }
+
+    @Override
+    protected String getJSMainModuleName() {
+      return "index";
     }
   };
 
