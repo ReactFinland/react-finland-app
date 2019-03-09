bf0a4c7a6984ed64772b3e30ab01aae973822ac0
Toni Ristola <toni.ristola@robu.fi>
2019-03-09 10:57:58 +0200

    updated libs and removed react-native-i18n

--- android/app/src/main/java/com/reactfinlandapp/MainApplication.java
+++ android/app/src/main/java/com/reactfinlandapp/MainApplication.java
@@ -3,7 +3,6 @@ package com.reactfinlandapp;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
-import com.AlexanderZaytsev.RNI18n.RNI18nPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.lugg.ReactNativeConfig.ReactNativeConfigPackage;
 import com.facebook.react.ReactNativeHost;
@@ -26,7 +25,6 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
             new MainReactPackage(),
-            new RNI18nPackage(),
             new VectorIconsPackage(),
             new ReactNativeConfigPackage()
       );
